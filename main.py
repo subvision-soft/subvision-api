202e5282d277475bf1bf7ccf16f744bc7f215ff5
Paul <paulcoignac@gmail.com>
2025-01-02 12:43:00 +0100

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fastapi.params import Cookie
 from jose import jwt, JWTError
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from target_detection import get_sheet_coordinates
+from target_detection import get_sheet_coordinates, process_image
 
 # Constants
 SECRET_KEY = secrets.token_urlsafe(32)
@@ -78,5 +78,26 @@ def detect_target(
             raise ValueError("Invalid image format.")
         # Perform your image processing here
         return get_sheet_coordinates(image)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Invalid image data: {str(e)}")
+
+@app.post("/target-score")
+def target_score(
+        request: ImageRequest,
+        authorization: str = Depends(validate_token)
+):
+    try:
+        # Decode the base64 image string
+        decoded_data = base64.b64decode(request.image_data)
+        # Convert bytes to NumPy array
+        np_array = np.frombuffer(decoded_data, dtype=np.uint8)
+        # Decode the image using OpenCV
+        image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        # save the image to disk
+        cv2.imwrite("image.jpg", image)
+        if image is None:
+            raise ValueError("Invalid image format.")
+        # Perform your image processing here
+        return process_image(image)
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Invalid image data: {str(e)}")
--- target_detection.py
+++ target_detection.py
@@ -1,26 +1,41 @@
 import math
 from enum import Enum
 import timeit
-
+from typing import Sequence
+import base64
 import cv2
 import numpy as np
 from ultralytics import YOLO
 import os
+
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 from numpy import ndarray
+
+Ellipse = tuple[Sequence[float], Sequence[float], float]
 class Zone(Enum):
     TOP_LEFT = 1
     TOP_RIGHT = 2
     BOTTOM_LEFT = 3
     BOTTOM_RIGHT = 4
     CENTER = 5
+
+class Impact:
+    def __init__(self, distance: int, score: int, zone: Zone, angle: float, amount: int):
+        self.distance = distance
+        self.score = score
+        self.zone = zone
+        self.angle = angle
+        self.amount = amount
+
+
 PICTURE_SIZE_SHEET_DETECTION = 1000
 model = YOLO("nano_semantic_model.pt")
 
 
 def to_radians(angle):
     return angle * math.pi / 180
+
 
 def rotate_point(center, point, angle):
     s = math.sin(angle)
@@ -41,13 +56,14 @@ def rotate_point(center, point, angle):
 
 
 def get_point_on_ellipse(ellipse, angle):
-    center, radii,ellipse_angle = ellipse
+    center, radii, ellipse_angle = ellipse
     radius_x, radius_y = radii
-    x = center[0] + math.cos(angle) * (radius_x /2)
-    y = center[1] + math.sin(angle) * (radius_y /2)
-    rotated_point = rotate_point(center, (x, y),to_radians(ellipse_angle) + to_radians(180))
+    x = center[0] + math.cos(angle) * (radius_x / 2)
+    y = center[1] + math.sin(angle) * (radius_y / 2)
+    rotated_point = rotate_point(center, (x, y), to_radians(ellipse_angle) + to_radians(180))
     # Convert the coordinates to integers
     return (int(x), int(y))
+
 
 def grow_ellipse(ellipse, factor):
     center, radii, angle = ellipse
@@ -57,10 +73,12 @@ def grow_ellipse(ellipse, factor):
 def get_distance(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
+
 def get_angle(point1, point2):
     return math.atan2(point2[1] - point1[1], point2[0] - point1[0])
 
-def get_real_distance(center, border, impact):
+
+def get_real_distance(center, border, impact) -> int:
     length = get_distance(center, border)
     distance = get_distance(center, impact)
     percent = distance / length
@@ -68,8 +86,9 @@ def get_real_distance(center, border, impact):
     millimeter_distance = real_length * percent
     return round(millimeter_distance)
 
-def get_points(distance):
-    point = 570
+
+def get_score(distance) -> int:
+    score = 570
     i = 0
     maximum_impact_distance = 48
     if distance > maximum_impact_distance:
@@ -77,16 +96,18 @@ def get_points(distance):
     for i in range(5):
         if distance <= 0:
             break
-        point -= 6
+        score -= 6
         distance -= 1
     for i in range(i, 48):
         if distance <= 0:
             break
-        point -= 3
+        score -= 3
         distance -= 1
-    return point
+    return score
 
-def draw_cross_at_coordinates(image: np.ndarray, coordinates: list, color: tuple = (0, 255, 0), size: int = 20, width: int = 2):
+
+def draw_cross_at_coordinates(image: np.ndarray, coordinates: list, color: tuple = (0, 255, 0), size: int = 20,
+                              width: int = 2):
     for coordinate in coordinates:
         # Convert coordinates to integers in case they are floats
         x, y = int(coordinate[0]), int(coordinate[1])
@@ -97,6 +118,8 @@ def draw_cross_at_coordinates(image: np.ndarray, coordinates: list, color: tuple
             cv2.line(image, (x - size, y), (x + size, y), color, width)
             # Draw vertical line of the cross
             cv2.line(image, (x, y - size), (x, y + size), color, width)
+
+
 def enhance_image_for_edge_detection(image: ndarray, blur_radius: int = 0):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     h, saturation, v = cv2.split(hsv)
@@ -110,6 +133,7 @@ def get_edges(image: ndarray, blur_radius: int = 0, canny_threshold_1: int = 100
     if blur_radius > 0:
         image_clone = cv2.blur(image_clone, (blur_radius, blur_radius))
     return cv2.Canny(image_clone, canny_threshold_1, canny_threshold_2)
+
 
 def clamp(value, min_value=-1.0, max_value=1.0):
     return max(min_value, min(value, max_value))
@@ -150,15 +174,15 @@ def get_sheet_coordinates(sheet_mat: ndarray):
     mat_resized = cv2.resize(sheet_mat.copy(), (PICTURE_SIZE_SHEET_DETECTION, PICTURE_SIZE_SHEET_DETECTION))
     results = model.track(mat_resized, verbose=False, conf=0.5)
     if results[0].masks is not None:
-        #get index of best detection
-        best_detection_index =np.argsort(results[0].boxes.conf)[-1:]
+        # get index of best detection
+        best_detection_index = np.argsort(results[0].boxes.conf)[-1:]
         mask = results[0].masks.data.cpu().numpy()[best_detection_index]
         # Convert mask to 8-bit single-channel image
         mask = (mask * 255).astype(np.uint8)
         mask = cv2.resize(mask, (PICTURE_SIZE_SHEET_DETECTION, PICTURE_SIZE_SHEET_DETECTION))
         contours, _ = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
         biggest_contour = get_biggest_valid_contour(contours)
-        #draw the contour
+        # draw the contour
         if biggest_contour is None:
             return None
 
@@ -169,25 +193,19 @@ def get_sheet_coordinates(sheet_mat: ndarray):
             PICTURE_SIZE_SHEET_DETECTION
         )
     return None
+
+
 def extract_sheet(image):
-    kernel = np.ones((5,5),np.uint8)
-    img = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations= 3)
-    mask = np.zeros(img.shape[:2],np.uint8)
-    bgdModel = np.zeros((1,65),np.float64)
-    fgdModel = np.zeros((1,65),np.float64)
-    rect = (20,20,img.shape[1]-20,img.shape[0]-20)
+    kernel = np.ones((5, 5), np.uint8)
+    img = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=3)
+    mask = np.zeros(img.shape[:2], np.uint8)
+    bgdModel = np.zeros((1, 65), np.float64)
+    fgdModel = np.zeros((1, 65), np.float64)
+    rect = (20, 20, img.shape[1] - 20, img.shape[0] - 20)
     cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
-    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-    img = img*mask2[:,:,np.newaxis]
+    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+    img = img * mask2[:, :, np.newaxis]
     return img
-
-
-
-
-
-
-
-
 
 
 def draw_sheet_detection_contour(image, coordinates):
@@ -202,7 +220,8 @@ def percentage_to_coordinates(percentage_coordinates, width, height):
         coordinates.append((int(percentage_coordinate[0] * width), int(percentage_coordinate[1] * height)))
     return coordinates
 
-def get_sheet_picture(image: ndarray):
+
+def get_sheet_picture(image: ndarray)->ndarray:
     coordinates = get_sheet_coordinates(image)
     if coordinates is None:
         return None
@@ -217,6 +236,7 @@ def get_sheet_picture(image: ndarray):
     transformation_matrix = cv2.getPerspectiveTransform(approx, target_coordinates)
     return cv2.warpPerspective(image, transformation_matrix,
                                (PICTURE_SIZE_SHEET_DETECTION, PICTURE_SIZE_SHEET_DETECTION))
+
 
 def get_crop_coordinates(image: ndarray, target_zone: Zone):
     height, width, _ = image.shape
@@ -237,26 +257,13 @@ def get_crop_coordinates(image: ndarray, target_zone: Zone):
     return {'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2}
 
 
-def get_target_picture(sheet_mat: ndarray, target_zone: Zone):
+def get_target_picture(sheet_mat: ndarray, target_zone: Zone)->ndarray:
     sheet_mat_clone = sheet_mat.copy()
     coordinates = get_crop_coordinates(sheet_mat_clone, target_zone)
     return sheet_mat_clone[coordinates['x1']:coordinates['x2'], coordinates['y1']:coordinates['y2']]
 
 
-def get_biggest_contour(contours):
-    max_area = 0
-    biggest_contour = None
-    for contour in contours:
-        bounding_rect = cv2.boundingRect(contour)
-        area = bounding_rect[2] * bounding_rect[3]
-        if area > max_area:
-            max_area = area
-            biggest_contour = contour
-    return biggest_contour
-
-
-def get_impacts_mask(image: ndarray):
-
+def get_impacts_mask(image: ndarray)->ndarray:
     image = cv2.blur(image.copy(), (PICTURE_SIZE_SHEET_DETECTION // 100, PICTURE_SIZE_SHEET_DETECTION // 100))
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     h, saturation, v = cv2.split(hsv)
@@ -277,6 +284,7 @@ def get_impacts_mask(image: ndarray):
     cv2.drawContours(mask, contours, -1, 255, -1)  # Draw filled contour in mask
     return mask
 
+
 def get_impacts_coordinates(image: ndarray) -> list:
     mask = get_impacts_mask(image)
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -287,8 +295,6 @@ def get_impacts_coordinates(image: ndarray) -> list:
     ellipses = [ellipse for ellipse in ellipses if not math.isnan(ellipse[0][0]) and not math.isnan(ellipse[0][1])]
     centers = [(int(ellipse[0][0]), int(ellipse[0][1])) for ellipse in ellipses]
     return centers
-
-
 
 
 def get_color_mask(mat: ndarray, color: tuple):
@@ -304,7 +310,9 @@ def get_color_mask(mat: ndarray, color: tuple):
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
     _, mask = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)
     return mask
-def get_target_ellipse(mat,zone:Zone):
+
+
+def get_target_ellipse(mat)->Ellipse:
     circle = np.zeros((mat.shape[1], mat.shape[0]), dtype=np.uint8)
     cv2.circle(circle, (mat.shape[1] // 2, mat.shape[0] // 2), int(mat.shape[1] / 2.2), (255, 255, 255), -1)
 
@@ -345,13 +353,14 @@ def get_target_ellipse(mat,zone:Zone):
     return ellipse
 
 
-def get_targets_ellipse(image: ndarray):
+def get_targets_ellipse(image: ndarray)->dict[Zone, Ellipse]:
     zones = [Zone.TOP_LEFT, Zone.TOP_RIGHT, Zone.CENTER, Zone.BOTTOM_LEFT, Zone.BOTTOM_RIGHT]
     ellipsis = {}
     for zone in zones:
         target_mat = get_target_picture(image, zone)
-        ellipsis[zone] = get_target_ellipse(target_mat,zone)
+        ellipsis[zone] = get_target_ellipse(target_mat)
     return ellipsis
+
 
 def target_coordinates_to_sheet_coordinates(ellipsis: dict):
     new_ellipsis = {}
@@ -374,18 +383,16 @@ def target_coordinates_to_sheet_coordinates(ellipsis: dict):
     return new_ellipsis
 
 
-
-def process_image(image: ndarray):
+def process_image(image: ndarray)->tuple[bytes, list[Impact]] or None:
     sheet_mat = get_sheet_picture(image)
     if sheet_mat is None:
-        return None, None
+        return None
     coordinates = get_targets_ellipse(sheet_mat)
     coordinates = target_coordinates_to_sheet_coordinates(coordinates)
     impacts = get_impacts_coordinates(sheet_mat)
 
-    #iterate key and value
+    # iterate key and value
     for key, ellipse_contrat in coordinates.items():
-
         drawing_width = 2
 
         # Draw the ellipse
@@ -403,8 +410,6 @@ def process_image(image: ndarray):
         cv2.ellipse(sheet_mat, ellipse_moyen_blanc, target_color, drawing_width)
         cv2.ellipse(sheet_mat, ellipse_grand_blanc, target_color, drawing_width)
 
-
-
         top_point = get_point_on_ellipse(ellipse_cross_tip, to_radians(90))
         bottom_point = get_point_on_ellipse(ellipse_cross_tip, to_radians(270))
         left_point = get_point_on_ellipse(ellipse_cross_tip, to_radians(180))
@@ -412,13 +417,13 @@ def process_image(image: ndarray):
         cv2.line(sheet_mat, top_point, bottom_point, target_color, drawing_width)
         cv2.line(sheet_mat, left_point, right_point, target_color, drawing_width)
 
-    draw_cross_at_coordinates(sheet_mat, impacts,color=(0,165,255),width=3)
-
+    draw_cross_at_coordinates(sheet_mat, impacts, color=(0, 165, 255), width=3)
+    points: list[Impact] = []
     for impact in impacts:
         min_distance = float('inf')
         closest_zone = None
 
-        for key,coordinate in coordinates.items():
+        for key, coordinate in coordinates.items():
             distance = get_distance(impact, coordinate[0])
             if min_distance > distance:
                 min_distance = distance
@@ -446,11 +451,11 @@ def process_image(image: ndarray):
             point_on_ellipse,
             impact
         )
-        points = get_points(real_distance)
+        score = get_score(real_distance)
 
         cv2.putText(
             sheet_mat,
-            str(points),
+            str(score),
             impact,
             cv2.FONT_HERSHEY_SIMPLEX,
             1,
@@ -459,14 +464,22 @@ def process_image(image: ndarray):
         )
         cv2.putText(
             sheet_mat,
-            str(points),
+            str(score),
             impact,
             cv2.FONT_HERSHEY_SIMPLEX,
             1,
             [255, 255, 255, 255],
             3
         )
-    return coordinates
+        points.append(Impact(real_distance, score, closest_zone, rad_angle, 1))
+
+
+    # Encode the image to PNG format
+    _, buffer = cv2.imencode('.png', sheet_mat)
+    # Convert the buffer to a base64 string
+    base64_string = base64.b64encode(buffer.tobytes()).decode('utf-8')
+    # Add the MIME type prefix
+    return f"data:image/png;base64,{base64_string}", points
 #
 # image = cv2.imread('images/sheet.jpg')
 # numruns = 10
@@ -516,6 +529,3 @@ def process_image(image: ndarray):
 #
 # duration = timeit.Timer('method_name()', globals=globals()).timeit(1)
 # print(f"Duration: {duration / 1:.2f} seconds")
-
-
-
