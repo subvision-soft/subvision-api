53e54a7ea74ca42cac6d3da9136c6d910872d439
loic <loic@code-rhapsodie.fr>
2025-03-05 09:46:48 +0100

    #7 add metrics

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routers.dependencies import validate_token
 from routers.private import target
-from routers.public import token, news, metric
+from routers.public import token, news, metrics
 
 app = FastAPI()
 app.add_middleware(
